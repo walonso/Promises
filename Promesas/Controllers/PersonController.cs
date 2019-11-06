f74babef9b7c9e0bb56fb7b30f1ee4716025542f
walonsor <walteralonso20@yahoo.com>
2019-11-05 20:58:43 -0500

    understanding promises

---
+++ Promesas/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Promesas.Model;
+
+namespace Promesas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<bool> Save()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return true;
+        }
+
+        [HttpPut]
+        public string SaveImage()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return "saved image";
+        }
+
+        [HttpGet]
+        public ActionResult<Person> Get(int id)
+        {
+            System.Threading.Thread.Sleep(5000);
+            return new Person
+            {
+                Name = "name1",
+                LastName = "lastname 1"
+            };
+        }
+    }
+}
--- Promesas/Startup.cs
+++ Promesas/Startup.cs
@@ -25,12 +25,18 @@ namespace Promesas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
